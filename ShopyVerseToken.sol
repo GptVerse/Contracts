a0f2d6d663134ef1e73815d263d313c04e12de5a
hasanozkul <hasanozkul@gmail.com>
2022-09-20 13:16:33 +0300

    Curly bracker added

--- ShopyVerseToken.sol
+++ ShopyVerseToken.sol
@@ -6,4 +6,5 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract ShopyVerseToken is ERC20 {
     constructor() ERC20("ShopyVerse Token", "SHPV") {
         _mint(msg.sender, 3000000000*10**18);
+    }
 }
